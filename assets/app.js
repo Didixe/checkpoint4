c0125438a425f7399805ab912fa65ee4cc22c4f6
Didixe <Adeline.gilet@gmail.com>
2024-01-31 16:19:01 +0100

    home

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,6 @@
 import './bootstrap.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.min.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -6,5 +8,7 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+
+require('bootstrap');
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
--- importmap.php
+++ importmap.php
@@ -27,4 +27,17 @@ return [
     '@hotwired/turbo' => [
         'version' => '7.3.0',
     ],
+    'bootstrap/dist/css/bootstrap.min.css' => [
+        'version' => '5.3.2',
+        'type' => 'css',
+    ],
+    'bootstrap' => [
+        'version' => '5.3.2',
+    ],
+    '@popperjs/core' => [
+        'version' => '2.11.8',
+    ],
+    'bootstrap/dist/js/bootstrap.min.js' => [
+        'version' => '5.3.2',
+    ],
 ];
