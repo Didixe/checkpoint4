82a05091d46245d80575354bc5933714a9237437
Didixe <Adeline.gilet@gmail.com>
2024-02-01 11:59:46 +0100

    page admin instrument delete/modify/add

--- src/Entity/Instrument.php
+++ src/Entity/Instrument.php
@@ -3,8 +3,14 @@
 namespace App\Entity;
 
 use App\Repository\InstrumentRepository;
+use DateTime;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\Validator\Constraints as Assert;
 
+#[Vich\Uploadable]
 #[ORM\Entity(repositoryClass: InstrumentRepository::class)]
 class Instrument
 {
@@ -26,7 +32,14 @@ class Instrument
     private ?int $Note_Number = null;
 
     #[ORM\Column(length: 255)]
-    private ?string $Picture = null;
+    private ?string $picture = null;
+
+    #[Vich\UploadableField(mapping: 'picture', fileNameProperty: 'picture')]
+    #[Assert\File(maxSize: '1M', mimeTypes: ['image/jpeg', 'image/jpg', 'image/png', 'image/webp'])]
+    private ?File $pictureFile = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $updatedAt = null;
 
     #[ORM\Column(length: 255)]
     private ?string $Price = null;
@@ -95,12 +108,40 @@ class Instrument
 
     public function getPicture(): ?string
     {
-        return $this->Picture;
+        return $this->picture;
     }
 
-    public function setPicture(string $Picture): static
+    public function setPicture(string $picture): static
     {
-        $this->Picture = $Picture;
+        $this->picture = $picture;
+
+        return $this;
+    }
+
+    public function getPictureFile(): ?File
+    {
+        return $this->pictureFile;
+    }
+
+    public function setPictureFile(File $image = null): static
+    {
+        $this->pictureFile = $image;
+
+        if ($image) {
+            $this->updatedAt = new DateTime('now');
+        }
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
 
         return $this;
     }
