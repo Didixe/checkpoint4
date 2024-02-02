56ea2c0116402ac2cf05db522f5208ed89fddb2c
Didixe <Adeline.gilet@gmail.com>
2024-02-02 11:38:54 +0100

    admin-comment

---
+++ src/Form/Comment1Type.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Client;
+use App\Entity\Comment;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Comment1Type extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('object')
+            ->add('Message')
+            ->add('client', EntityType::class, [
+                'class' => Client::class,
+'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Comment::class,
+        ]);
+    }
+}
